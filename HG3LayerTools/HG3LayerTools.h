3c3f5f186ef8cba9adf7922ad2a61a64551cfe7a
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2015-05-28 09:55:13 -0700

    Initial Commit

---
+++ HG3LayerTools/HG3LayerTools.h
@@ -0,0 +1,19 @@
+//
+//  HG3LayerTools.h
+//  HG3LayerTools
+//
+//  Created by Hugo Gonzalez on 5/28/15.
+//  Copyright (c) 2015 HG3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HG3LayerTools.
+FOUNDATION_EXPORT double HG3LayerToolsVersionNumber;
+
+//! Project version string for HG3LayerTools.
+FOUNDATION_EXPORT const unsigned char HG3LayerToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HG3LayerTools/PublicHeader.h>
+
+
