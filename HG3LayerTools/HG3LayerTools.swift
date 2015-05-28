122afe7d7ea35a8f1f66f4a62b27342b7100c4e3
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2015-05-28 15:34:32 -0700

    Created HG3LayerTools File

---
+++ HG3LayerTools/HG3LayerTools.swift
@@ -0,0 +1,30 @@
+//
+//  HG3LayerTools.swift
+//  HG3LayerTools
+//
+//  Created by Hugo Gonzalez on 5/28/15.
+//  Copyright (c) 2015 HG3. All rights reserved.
+//
+
+import Foundation
+
+public extension UIView {
+    enum LayerProperties {
+        case CornerRadius, BorderColor, BorderWidth
+    }
+    
+    //MARK: Public Functions
+    func setLayerProperties(layerProperties: [LayerProperties: AnyObject]) {
+        if let val = layerProperties[.BorderWidth] as? CGFloat {
+           self.layer.borderWidth = val
+        }
+        
+        if let val = layerProperties[.BorderColor] as? UIColor {
+            self.layer.borderColor = val.CGColor
+        }
+        
+        if let val = layerProperties[.CornerRadius] as? CGFloat {
+            self.layer.cornerRadius = val
+        }
+    }
+}
